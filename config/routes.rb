edf633b6f21d1b8a0d0db58251cc579f0f1fad90
Adam Tse <mr.adam.tse@gmail.com>
2016-09-25 00:30:41 -0400

    created items view/item routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root "items#index"
   devise_for :users
 
   resources :items
-  root 'items#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
