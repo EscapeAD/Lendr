8da5fab2b6e3015bd9be5646ed13a73c0ad7741d
Adam Tse <mr.adam.tse@gmail.com>
2016-09-25 00:54:32 -0400

    created devise views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user  = User.new(user_params)
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name,:last_name)
   end
 end
