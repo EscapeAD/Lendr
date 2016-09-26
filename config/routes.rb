3e74d95d4362b3baad730cf2ffab67fd5f939546
Adam Tse <mr.adam.tse@gmail.com>
2016-09-26 17:26:56 -0400

    added authenticate user for user show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "items#index"
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  resource :user, only: [:show]
 
   resources :items
 
