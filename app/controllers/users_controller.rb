e31aa291f65fbf5d685713b7a0731a2abc84da0c
Adam Tse <mr.adam.tse@gmail.com>
2016-09-26 16:16:01 -0400

    removed user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,27 +4,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    @user.update_attributes(user_params)
-    if @user.save
-    else
-      render :edit
-    end
-
-  end
 
 
   private
