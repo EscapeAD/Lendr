02654f23f39d16c652721fe16c50170bdb1c5bb8
Adam Tse <mr.adam.tse@gmail.com>
2016-09-26 19:18:56 -0400

    add inventory user class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
+  def self.count_inventory(user_id)
+    inventory       = 0
+    inventory_total = Items.where(owner_id: user_id)
+    inventory.each do |x|
+      inventory += 1
+    end
+    return inventory
+  end
+
 end
