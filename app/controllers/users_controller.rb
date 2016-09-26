8f539df0486a48eec5edf9741305cb4a57bc7960
Adam Tse <mr.adam.tse@gmail.com>
2016-09-26 16:18:13 -0400

    add email first/last/city to form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,5 @@ class UsersController < ApplicationController
 
 
 
-  private
-  def user_params
-    params.require(:user).permit(:first_name,:last_name,:password,:password_confirmation,:city)
-  end
+
 end
