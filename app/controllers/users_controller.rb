13d32b43abdbd92d5ad090f0cf622f509931501c
Adam Tse <mr.adam.tse@gmail.com>
2016-09-26 13:22:56 -0400

    devise strong param changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name,:last_name)
+    params.require(:user).permit(:first_name,:last_name,:password,:password_confirmation,:city)
   end
 end
