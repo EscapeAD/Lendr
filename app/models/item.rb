6d77a2ac31c0493a06010bcba6bcb6faae7f9c27
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-09-26 20:42:44 -0400

    changed item model and user associations; added new item form

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,2 @@
 class Item < ApplicationRecord
-  belongs_to :owner, class_name: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924225652) do
+ActiveRecord::Schema.define(version: 20160927003732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,12 +30,12 @@ ActiveRecord::Schema.define(version: 20160924225652) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
-    t.string   "type"
     t.text     "description"
-    t.integer  "owner_id"
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "item_type"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
