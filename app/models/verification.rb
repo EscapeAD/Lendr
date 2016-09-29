41f1d92357462924914020334b4811925c2fdab2
Adam Tse <mr.adam.tse@gmail.com>
2016-09-29 10:42:42 -0400

    conditional added verfication.verify user

--- app/models/verification.rb
+++ app/models/verification.rb
@@ -6,4 +6,15 @@ class Verification < ApplicationRecord
     Verification.where(checkout_id: input_id, status: 'pickup')[0][:id]
   end
 
+  def self.verify_user(itemId, checkoutsId, verfId)
+    item_owner           = Item.find(itemId)
+    check                = Checkout.find(checkoutsId)
+    verification_session = Verification.find(verfId)
+    if current_user.id == item_owner.id
+      verification_session.update_attribute(:lender, true)
+    else
+      verification_session.update_attribute(:borrower, true)
+    end
+  end
+
 end
