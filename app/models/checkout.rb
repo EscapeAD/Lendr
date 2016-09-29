e5cb407fcb5d7df30c3f8c4d13c262e2bbec392d
Adam Tse <mr.adam.tse@gmail.com>
2016-09-28 20:34:59 -0400

    verfication added for check if lent or borrowed

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -9,7 +9,8 @@ class Checkout < ApplicationRecord
     list = Checkout.where(user_id: user_id)
     final = []
     list.each do | item |
-      if item[:check_initial] == true
+      if item[:check_initial] == true && item[:due_date] != nil
+        list << item
         final << item
       end
     end
@@ -22,7 +23,7 @@ class Checkout < ApplicationRecord
     checkout  = Checkout.all
     checkout.each do |item|
       inventory.each do |invent_item|
-        if item[:item_id] == invent_item[:id] && item[:returned] == false && item[:check_initial] == true
+        if item[:item_id] == invent_item[:id] && item[:returned] == false && item[:check_initial] == true && item[:due_date] != nil
           list << item
         end
       end
