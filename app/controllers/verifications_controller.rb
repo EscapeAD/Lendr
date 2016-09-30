ef3877db3e672bdaae83d11ccd20749db07669ae
Adam Tse <mr.adam.tse@gmail.com>
2016-09-29 23:38:07 -0400

    correct bug with users, renamed show verfication to owner_item

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -2,11 +2,7 @@ class VerificationsController < ApplicationController
   def show
     @verify = Verification.find(params[:id])
     @status_text = Verification.status_text(params[:id])
-    @user   = Item.find(params[:item_id])
-
-
-
-
+    @owner_of_item   = Item.find(params[:item_id])
   end
 
   def new
@@ -19,11 +15,11 @@ class VerificationsController < ApplicationController
     Verification.verify_user(params[:item_id],params[:checkout_id],params[:id],current_user)
 
     Verification.verify_staging(@verify.id)
-    redirect_to user_path
 
     if @verify == nil
       redirect_to user_path
     end
+
 
 
   end
