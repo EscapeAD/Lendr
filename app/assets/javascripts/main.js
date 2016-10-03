88f1bd9a7f3cb70c0a386beaf2d197d8c14f7a91
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-10-03 18:40:51 -0400

    revised javascript for google maps

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,21 @@
+handler = Gmaps.build('Google');
+handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+  markers = handler.addMarkers([
+    {
+      "lat": 0,
+      "lng": 0,
+      "picture": {
+        "url": "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
+        "width":  32,
+        "height": 32
+      },
+      "infowindow": "hello!"
+    }
+  ]);
+  handler.bounds.extendWith(markers);
+  handler.fitMapToBounds();
+});
+
 $(document).on('turbolinks:load', function(){
   // $('.confirm').on('click', function(event){
   //   var checkoutId = $(this).data('checkout');
@@ -12,7 +30,6 @@ $(document).on('turbolinks:load', function(){
   //   }).done(function(rData){
   //   });
   // });
-
 
   $('#searchTextField').keypress(function(e){
     if (e.which === 13) {
@@ -46,23 +63,4 @@ $(document).on('turbolinks:load', function(){
       location.reload();
     });
   });
-
-  handler = Gmaps.build('Google');
-  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-    markers = handler.addMarkers([
-      {
-        "lat": 0,
-        "lng": 0,
-        "picture": {
-          "url": "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
-          "width":  32,
-          "height": 32
-        },
-        "infowindow": "hello!"
-      }
-    ]);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-  });
-
 });
