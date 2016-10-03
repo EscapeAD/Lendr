438826bb7d8b4524bef325190448783e9e74201a
Adam Tse <mr.adam.tse@gmail.com>
2016-10-03 00:36:09 -0400

    update method and story params created

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,16 @@
 class StoriesController < ApplicationController
+
   def update
+    @story = Story.find(params[:id])
+    @story.update_attributes(story_params)
+    @story.save
+    @story.update_attributes(completed: true, datetime: Date.now)
+    @story.save
+    redirect_to user_path
+  end
+
+  private
+  def story_params
+    params.require(:story).permit(:title,:story)
   end
 end
