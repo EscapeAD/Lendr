0772737754d52f9468efb8c1d56c000efd834956
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-10-03 00:14:19 -0400

    added geocoding to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930202509) do
+ActiveRecord::Schema.define(version: 20161002195523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,11 +35,11 @@ ActiveRecord::Schema.define(version: 20160930202509) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "item_type"
-    t.integer  "user_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "stories", force: :cascade do |t|
@@ -68,6 +68,9 @@ ActiveRecord::Schema.define(version: 20160930202509) do
     t.string   "city"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "address"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
