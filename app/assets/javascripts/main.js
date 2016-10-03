d9baf96a0202f061a15dd528c6c68097d0ca9733
Adam Tse <mr.adam.tse@gmail.com>
2016-10-02 23:29:33 -0400

    refactor code

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,17 +1,17 @@
 $(document).on('turbolinks:load', function(){
-  $('.confirm').on('click', function(event){
-    var checkoutId = $(this).data('checkout');
-    var  itemId    = $(this).data('inventory');
-    console.log(itemId);
-    console.log(checkoutId);
-    $.ajax({
-      url: '/items/'+itemId+'/checkouts/'+checkoutId,
-      method: 'PUT',
-      data: {id: checkoutId},
-      dataType: 'JSON'
-    }).done(function(rData){
-    });
-  });
+  // $('.confirm').on('click', function(event){
+  //   var checkoutId = $(this).data('checkout');
+  //   var  itemId    = $(this).data('inventory');
+  //   console.log(itemId);
+  //   console.log(checkoutId);
+  //   $.ajax({
+  //     url: '/items/'+itemId+'/checkouts/'+checkoutId,
+  //     method: 'PUT',
+  //     data: {id: checkoutId},
+  //     dataType: 'JSON'
+  //   }).done(function(rData){
+  //   });
+  // });
 
 
   $('#searchTextField').keypress(function(e){
