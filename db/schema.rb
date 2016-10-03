54e0bf9f94c3c61d5effa75a5a7743bf4cd906a2
Victor McLean <vmvictorvm@gmail.com>
2016-10-03 16:32:44 -0400

    Implemented Category model, remove item_type from Item Model, Add drop-down list for category in the form

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003174334) do
+ActiveRecord::Schema.define(version: 20161003175947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "checkouts", force: :cascade do |t|
     t.integer  "user_id"
@@ -35,6 +41,8 @@ ActiveRecord::Schema.define(version: 20161003174334) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
   create_table "pictures", force: :cascade do |t|
@@ -95,6 +103,7 @@ ActiveRecord::Schema.define(version: 20161003174334) do
 
   add_foreign_key "checkouts", "items"
   add_foreign_key "checkouts", "users"
+  add_foreign_key "items", "categories"
   add_foreign_key "pictures", "items"
   add_foreign_key "stories", "checkouts"
   add_foreign_key "verifications", "checkouts"
