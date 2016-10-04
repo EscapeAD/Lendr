3d8e75455fb487dc52576e8e6906d2c7d0adff8d
Adam Tse <mr.adam.tse@gmail.com>
2016-10-03 20:56:11 -0400

    fixed schema and schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 20161003234713) do
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.string   "first_name"
     t.string   "last_name"
