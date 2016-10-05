37a37608a46a8987b76db7ca3ad53b4ef955870a
Adam Tse <mr.adam.tse@gmail.com>
2016-10-05 11:35:06 -0400

    basic messaging up and running without websocket

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
   end
 
   resources :mails
+  resources :messages, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
