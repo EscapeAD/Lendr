a264cdb1fc47fc54811337fdff4129bd0580eb14
Adam Tse <mr.adam.tse@gmail.com>
2016-10-07 01:52:06 -0400

    fixing a syntax error

--- app/models/story.rb
+++ app/models/story.rb
@@ -6,11 +6,12 @@ class Story < ApplicationRecord
 
   def self.story_check(checkout_user_id)
     story = Story.find_by(checkout_id: checkout_user_id)
-    if story.nil?
-      return false
-    elsif story.completed == false
-      return true
-    else
-      return false
+      if story.nil?
+        return false
+      elsif story.completed == false
+        return true
+      else
+        return false
+      end
     end
 end
