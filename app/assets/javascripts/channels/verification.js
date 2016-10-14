73f83cda29d2906d5ccb00b1610cf3f23118d0ad
Adam Tse <mr.adam.tse@gmail.com>
2016-10-14 16:06:01 -0400

    moved scrolldown to chat only/on append

--- app/assets/javascripts/channels/verification.js
+++ app/assets/javascripts/channels/verification.js
@@ -1,18 +1,18 @@
 $(document).on('turbolinks:load', function(){
-  var messages, messages_to_bottom;
-  messages = $('.chat');
-  if ($('.chat').length > 0) {
-    messages_to_bottom = function() {
-      return messages.scrollTop(messages.prop("scrollHeight"));
-    };
-    messages_to_bottom();
-  }
 
 App.verification = App.cable.subscriptions.create({
   channel:'VerificationChannel',
    room: window.location.pathname.split("/")[6]
  }, {
   received: function(data){
+    var messages, messages_to_bottom;
+    messages = $('.chat');
+    if ($('.chat').length > 0) {
+      messages_to_bottom = function() {
+        return messages.scrollTop(messages.prop("scrollHeight"));
+      };
+      messages_to_bottom();
+    }
     $('#new_message').append(data.message).get(0).focus();
     $('#enterChat').val('');
     messages_to_bottom();
