7be736811f72fdc28432582b49860cfc4ce5278a
Adam Tse <mr.adam.tse@gmail.com>
2016-11-28 23:08:52 -0500

    corrected some unit test items

--- test/controllers/mailboxes_controller_test.rb
+++ test/controllers/mailboxes_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class MailboxesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get mailboxes_url
+    get mailboxes_path
     assert_response :success
   end
 
   test "should get show" do
-    get mailbox_url
+    get mailbox_path
     assert_response :success
   end
 
